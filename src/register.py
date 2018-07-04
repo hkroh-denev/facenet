e4d28886f18827150162370ccbe27f7eb0d2b731
Hyungki Roh <hkroh.denev@gmail.com>
2018-07-04 14:56:53 +0900

    add register script (test)

---
+++ src/register.py
@@ -0,0 +1,154 @@
+import sys
+import argparse
+
+import tensorflow as tf
+import numpy as np
+
+import facenet2
+import align.detect_face
+
+from scipy import misc
+import pickle
+
+
+import cv2
+
+print(cv2.__version__)
+
+def cam_test():
+    cap = cv2.VideoCapture(0)
+    #cap.set(1, 0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('video capture', frame)
+        ch = 0xff & cv2.waitKey(1)
+        if ch == 27:
+            break
+
+class MTCNN:
+    def __init__(self, sess):
+        self.session = sess
+        self.pnet, self.rnet, self.onet = align.detect_face.create_mtcnn(self.session, None)
+
+        self.minsize = 20 # minimum size of face
+        self.threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
+        self.factor = 0.709 # scale factor
+
+        self.margin_rate = 0.5
+        self.image_size = 120 * 2
+
+    def detect(self, img):
+        with self.session.as_default():
+            if img.ndim<2:
+                return [], []
+            if img.ndim == 2:
+                img = facenet2.to_rgb(img)
+            img = img[:,:,0:3]
+
+            bounding_boxes, f_points = align.detect_face.detect_face(img, self.minsize, self.pnet, self.rnet, self.onet, self.threshold, self.factor)
+            print('bounding_boxes', bounding_boxes)
+            nrof_faces = bounding_boxes.shape[0]
+            detected_faces = []
+            detected_bb = []
+            face_score = []
+            if nrof_faces>0:
+                det = bounding_boxes[:,0:4]
+                face_score = bounding_boxes[:, 4]
+                det_arr = []
+                img_size = np.asarray(img.shape)[0:2]
+                #if nrof_faces>1: #??
+                for i in range(nrof_faces):
+                    det_arr.append(np.squeeze(det[i]))
+
+                for i, det in enumerate(det_arr):
+                    det = np.squeeze(det)
+                    bb = np.zeros(4, dtype=np.int32)
+                    margin0 = (det[2] - det[0]) * self.margin_rate
+                    margin1 = (det[3] - det[1]) * self.margin_rate
+                    bb[0] = np.maximum(det[0]-margin0, 0)
+                    bb[1] = np.maximum(det[1]-margin1, 0)
+                    bb[2] = np.minimum(det[2]+margin0, img_size[1])
+                    bb[3] = np.minimum(det[3]+margin1, img_size[0])
+                    cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                    scaled = misc.imresize(cropped, (self.image_size, self.image_size), interp='bilinear')
+                    detected_faces.append(scaled)
+                    detected_bb.append(bb)
+
+                    for j in range(10):
+                        if j < 5:
+                            f_points[j, i] = (f_points[j, i] - bb[0]) / (bb[2] - bb[0])
+                        else:
+                            f_points[j, i] = (f_points[j, i] - bb[1]) / (bb[3] - bb[1])
+
+            return detected_faces, detected_bb, f_points, face_score
+
+def face_alignment(img, face_size, f_point):
+    desired_left_eye = (0.35, 0.35)
+    desired_right_eye = (0.65, 0.35)
+    right_eye_center = (f_point[0], f_point[5])
+    left_eye_center = (f_point[1], f_point[6])
+
+    eyes_center = ( (right_eye_center[0] + left_eye_center[0])/2 * img.shape[1], (right_eye_center[1] + left_eye_center[1])/2 * img.shape[0])
+    diff_x = np.sqrt(np.square(right_eye_center[0] - left_eye_center[0]))
+    diff_y = np.sqrt(np.square(right_eye_center[1] - left_eye_center[1]))
+    if left_eye_center[1] < right_eye_center[1]:
+        sign = -1.0
+    else:
+        sign = 1.0
+    angle = np.degrees(np.arctan2(diff_y, diff_x))
+    angle *= sign
+    scale = np.sqrt(np.square(desired_right_eye[0] - desired_left_eye[0]) + np.square(desired_right_eye[1] - desired_left_eye[1])) / np.sqrt(np.square(right_eye_center[0] - left_eye_center[0]) + np.square(right_eye_center[1] - left_eye_center[1]))
+
+    print(eyes_center, angle, scale)
+    M = cv2.getRotationMatrix2D(eyes_center, angle, scale)
+    tX = face_size * 0.5
+    tY = face_size * desired_left_eye[1]
+    M[0, 2] += (tX - eyes_center[0])
+    M[1, 2] += (tY - eyes_center[1])
+
+    (w, h) = (face_size, face_size)
+
+    return cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC)
+
+def extract_face_images(args):
+
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False)) as sess:
+        mtcnn = MTCNN(sess)
+
+        count = 0
+
+        vidcap = cv2.VideoCapture(args.input)
+        success, image = vidcap.read()
+        success = True
+        while success:
+            vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * args.interval))
+            success, image = vidcap.read()
+            if success:
+                detected_faces, detected_bb, f_points, face_score = mtcnn.detect(image)
+                rejected = 0
+                for i in range(len(detected_faces)):
+                    if face_score[i] > 0.9:
+                        aligned_face = face_alignment(detected_faces[i], mtcnn.image_size, f_points[:, i])
+                        aligned_face = misc.imresize(aligned_face, (args.image_size, args.image_size), interp='bicubic')
+                        cv2.imwrite(args.output + '/frame_%08d_%03dd.jpg' % (count, i), aligned_face)
+                    else:
+                        rejected += 1
+                print(count, 'face:', len(detected_faces), 'rejected:', rejected)
+            count += 1
+
+if __name__ == '__main__':
+    print('video to image')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='path to the input video file')
+    parser.add_argument('--output', help='path to the output images')
+    parser.add_argument('--interval', type=int, default=1000, help='saving interval in milliseconds')
+    parser.add_argument('--image_size', type=int, default=160, help='size of output image')
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=0.5)
+    args = parser.parse_args()
+    print(args)
+    extract_face_images(args)
